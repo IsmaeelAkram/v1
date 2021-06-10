404d3a80ea8b74c0947f90ee614d208d8fbde9b1
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-06-09 21:30:42 -0400

    Fix post hover light

--- pages/writing.js
+++ pages/writing.js
@@ -14,7 +14,7 @@ export default function Writing({ posts }) {
 					{posts.map((post) => {
 						return (
 							<Link href={`/post/${post.slug}`}>
-								<div className="transition bg-gray-100 dark:bg-gray-900 hover:dark:bg-gray-700 hover:bg-gray-100 cursor-pointer p-5 rounded-lg">
+								<div className="transition bg-gray-100 dark:bg-gray-900 hover:dark:bg-gray-700 hover:bg-gray-200 cursor-pointer p-5 rounded-lg">
 									<img src={post.feature_image} className="rounded-lg mb-5 object-cover h-40" />
 									<h3 className="text-2xl font-semibold mb-2">{post.title}</h3>
 									<p>{post.excerpt}</p>
