83bf49f6f2428b0d7ac36277bc33fede4d60ab02
Ismaeel <19865145+IsmaeelAkram@users.noreply.github.com>
2021-07-05 14:07:32 -0400

    Add note

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -11,7 +11,7 @@ export default function Posts(props) {
 			</Head>
 			<PageContainer title={props.title}>
 				<div
-					class="prose dark:prose-dark max-w-full w-4/6"
+					class="content prose dark:prose-dark max-w-full w-4/6"
 					dangerouslySetInnerHTML={{ __html: props.html }}
 				></div>
 			</PageContainer>
